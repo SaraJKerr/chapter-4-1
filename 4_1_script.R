5c8020ceabe584fc4e03df75e0f9e8eb8f3163d2
SaraJKerr <sarajkerr@me.com>
2017-10-04 09:12:53 +0100

    First commit

---
+++ 4_1_script.R
@@ -0,0 +1,9 @@
+# Code for Chapter 4 Term-Document Matrices, Section 1 Frequency Analysis
+
+# Requirements - folders containing texts in plain text format: AEO_corpus 
+# 19C_corpus
+
+# Results are saved in the 4_1_results and 4_1_plots folders
+
+# Load packages
+
